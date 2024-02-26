6085e92d479f043b86eb316ef71711e5e1004310
Chaewon <lcw3714@naver.com>
2024-02-27 01:21:09 +0900

    [Feat] 헤더에 마이페이지 버튼 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom';
 import './Header.scss';
 import { IoArrowBackCircleOutline } from 'react-icons/io5';
 import { FiHome } from 'react-icons/fi';
+import { IoPersonCircleOutline } from 'react-icons/io5';
 
 export default function Header() {
   const navigate = useNavigate();
@@ -13,10 +14,18 @@ export default function Header() {
   const homeButton = () => {
     navigate('/main');
   };
+
+  const myButton = () => {
+    navigate('/mypage');
+  };
+
   return (
     <div className="HeaderContainer">
       <IoArrowBackCircleOutline onClick={backButton} className="BackBtn" />
-      <FiHome onClick={homeButton} className="HomeBtn" />
+      <div>
+        <FiHome onClick={homeButton} className="HomeBtn" />
+        <IoPersonCircleOutline onClick={myButton} className="MyBtn" />
+      </div>
     </div>
   );
 }
