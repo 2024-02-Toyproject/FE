4ac8d865060ee216a3592860909ec8a13f8c745d
Chaewon <lcw3714@naver.com>
2024-05-10 22:10:31 +0900

    [Feat] 절세 상품 페이지 추가

---
+++ src/pages/product/TaxPage.jsx
@@ -0,0 +1,11 @@
+import Header from '../../components/Header';
+import './TaxPage.scss';
+
+export default function TaxPage() {
+  return (
+    <div>
+      <Header />
+      <h1>절세금융페이지입니다</h1>
+    </div>
+  );
+}
